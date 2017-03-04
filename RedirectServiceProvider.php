5b1a6214674061edf91ca8ed06fe65382fd70281
Eugene <zarkovsky@yandex.ru>
2017-03-04 12:48:51 +0300

    Added service provider

---
+++ RedirectServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Weblabspro\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RedirectServiceProvider extends ServiceProvider
+{
+    /**
+     * Determines if project is running not in backend, and if yes - redirects to backend
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if (!app()->runningInBackend() && !app()->runningInConsole() && !app()->runningUnitTests()) {
+            header('Location: /backend');
+
+            exit;
+        }
+    }
+}
